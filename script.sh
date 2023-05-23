9581b92a2f2bad1cba065afb8518a1ff00b3b74f
Sveta <svetakudakayeva@gmail.com>
2023-05-23 15:23:37 +0300

    adding script

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd HW
+mkdir folder1 folder2 folder3
+cd folder1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir folder4 folder5 folder6
+ls -la
+mv d:/HW/folder1/{file2.txt,file3.txt} d:/HW/folder2
+
+
